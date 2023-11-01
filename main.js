02dc2bb5b2a77db4c11bb16e4fd853fbac1365b2
NikoHOME <wiktor.gawronski@gmail.com>
2023-11-01 16:59:06 +0100

    Add progress bar and select promt

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import Xray from 'x-ray';
 const x = new Xray();
+
 class ItemPrice {
     constructor() {
         this.keys = 0;
@@ -11,23 +12,40 @@ class ItemPrice {
 class ScrapedItem {
     constructor() {
         this.name = "";
-        this.ballance = 0;
+        this.balance = 0;
+        this.buyOrder;
+        this.sellOrder;
     }
 }
 
 class ProgramMemory {
     constructor() {
+        this.scrapingLinksList = [];
         this.scrapingOutput = [];
+        this.scrapingItemCounter;
+        this.currentLink;
+        this.debug = false;
+        this.progressBar;
     }
+
+    decrementCounter() {
+        this.scrapingItemCounter -= 1;
+        this.progressBar.increment();
+
+        if(this.scrapingItemCounter == 0) {
+            process.emit("scrapingFinished");
+        }
+    }
+
 }
 
-let programMemory = new ProgramMemory();
+
 
 function compareScrapedItems(a, b ) {
-    if ( a.ballance < b.ballance ){
+    if ( a.balance < b.balance ){
       return -1;
     }
-    if ( a.ballance > b.ballance ){
+    if ( a.balance > b.balance ){
       return 1;
     }
     return 0;
@@ -72,126 +90,203 @@ function getKeyPrice() {
 }
 
 
+function processScrape(programMemory, err, result, link) {
 
 
+    if(err) {
+        //console.log(err);
+        if(programMemory.debug) console.log("<!!> Error for " + link);
+        programMemory.decrementCounter();
+        return;
+    }
 
-function scrapeLinks(linkList) {
+    if(result.length != 4) {
+        if(programMemory.debug) console.log("<!!> Scrape failed for " + link);
+        if(programMemory.debug) console.log("<||> Retrying " + link);
+        scrapeLink(programMemory, link);
+        return;
+        programMemory.scrapingItemCounter -= 1;
+        if(programMemory.scrapingItemCounter == 0) {
+            process.emit("scrapingFinished");
+        }
+        return;
+    }
 
-    //Couting scraped items;
-    let counter = linkList.length;
-    
-    for(let link of linkList) {
-        x(link, ".col-md-6", [{
-            title: "h4",
-            list: x( ".listing", [{
-                name: '.item@title',
-                value: '.item@data-listing_price',
-                comment: '.item@data-listing_comment',
-                //Check if specific elements exists to determine prefered trade type
-                friend_to_trade: x(".btn-primary", "i.fa-user-plus@class"), //blue icon + user
-                hagle_trade: x(".btn-primary", "i.fa-exchange@class"),      //blue icon + arrows
-                friend_to_trade_buyout: x(".btn-success", "i.fa-user-plus@class"), //green icon + user
-                buyout_trade: x(".btn-success", "i.fa-exchange@class"),     //green icon + arrows
-                instant_trade: x(".btn-success", "i.fa-flash@class"),       //green icon + flash
+    //Remove "Suggestion" section 
+    result = result.slice(0,2);
+    const filteredFields = [
+        "friend_to_trade", 
+        "friend_to_trade_buyout",
+        "spell",
+        "effect",
+    ];
+    for(let column of result) {
+        for(let item of column.list) {
+            // Remove friend to trade trades (can't automate them)
+            for(let field of filteredFields) {
+                if(Object.keys(item).indexOf(field) != -1) {
 
-                //Check if has any special modifiers
-                spell: '.item@data-spell_1',
-                effect: '.item@data-effect_name',
-
-            }])
-        }])
-        ((err, result) => {
-            
-            
-            if(err) {
-                console.log(err);
-                return;
-            }
-
-            if(result.length != 4) {
-                console.log("<!!> Scrape failed for " + link);
-                counter -= 1;
-                return;
-            }
-
-            //Remove "Suggestion" section 
-            result = result.slice(0,2);
-            const filteredFields = [
-                "friend_to_trade", 
-                "friend_to_trade_buyout",
-                "spell",
-                "effect",
-            ];
-            for(let column of result) {
-                for(let item of column.list) {
-                    // Remove friend to trade trades (can't automate them)
-                    for(let field of filteredFields) {
-                        if(Object.keys(item).indexOf(field) != -1) {
-
-                            column.list = column.list.filter( e => e !== item);
-                            continue;
-                        }
-                    }
-                    //Check if the item has normal currency listed otherwise remove it
-                    if(Object.keys(item).indexOf("value") == -1) {
-                        column.list = column.list.filter( e => e !== item);
-                        continue;
-                    }
-                    
+                    column.list = column.list.filter( e => e !== item);
+                    continue;
                 }
             }
-            
-            if(result[0].list.length == 0 || result[1].list.length == 0) {
-                console.log("<!!> Not enough items found, scrape failed for " + link);
-                counter -=1;
-                return;
+            //Check if the item has normal currency listed otherwise remove it
+            if(Object.keys(item).indexOf("value") == -1) {
+                column.list = column.list.filter( e => e !== item);
+                continue;
             }
-
-
-
-            //Pick the top offers (best prices)
             
-            let bestSell = result[0].list[0];
-            let bestBuy = result[1].list[0];
-            
-            //console.log(bestBuy);
-            //console.log(bestSell);
-            
-            let sellPrice = parseItemPrice(bestSell.value);
-            let buyPrice = parseItemPrice(bestBuy.value);
-
-            let refDiffence = getRefDifference(sellPrice, buyPrice);
-            
-            let scrapedItem = new ScrapedItem();
-
-            scrapedItem.name = result[0].list[0].name;
-            scrapedItem.ballance = parseFloat(refDiffence);
+        }
+    }
+    
+    if(result[0].list.length == 0 || result[1].list.length == 0) {
+        if(programMemory.debug) console.log("<!!> Not enough items found, scrape failed for " + link);
+        programMemory.decrementCounter();
+        return;
+    }
 
 
-            programMemory.scrapingOutput.push(scrapedItem);
-            counter -= 1;
-            console.log("<++> Scrape finished for " + link);
+
+    //Pick the top offers (best prices)
+    
+    let bestSell = result[0].list[0];
+    let bestBuy = result[1].list[0];
+    
+    //console.log(bestBuy);
+    //console.log(bestSell);
+    
+    let sellPrice = parseItemPrice(bestSell.value);
+    let buyPrice = parseItemPrice(bestBuy.value);
+
+    let refDiffence = getRefDifference(sellPrice, buyPrice);
+    
+    let scrapedItem = new ScrapedItem();
+
+    scrapedItem.name = result[0].list[0].name;
+    scrapedItem.balance = parseFloat(refDiffence);
+    scrapedItem.buyOrder = bestBuy;
+    scrapedItem.sellOrder = bestSell;
+
+    //console.log(programMemory.scrapingOutput);
+    programMemory.scrapingOutput.push(scrapedItem);
+
+    if(programMemory.debug) console.log("<++> Scrape finished for " + link);
 
 
-            if(counter == 0) {
-                process.emit("scrapingFinished");
-            }
-        });
+    programMemory.decrementCounter();
+}
+
+
+
+
+function scrapeLink(programMemory, link) {
+    x(link, ".col-md-6", [{
+        title: "h4",
+        list: x( ".listing", [{
+            name: '.item@title',
+            value: '.item@data-listing_price',
+            comment: '.item@data-listing_comment',
+            //Check if specific elements exists to determine prefered trade type
+            friend_to_trade: x(".btn-primary", "i.fa-user-plus@class"), //blue icon + user
+            hagle_trade: x(".btn-primary", "i.fa-exchange@class"),      //blue icon + arrows
+            friend_to_trade_buyout: x(".btn-success", "i.fa-user-plus@class"), //green icon + user
+            buyout_trade: x(".btn-success", "i.fa-exchange@class"),     //green icon + arrows
+            instant_trade: x(".btn-success", "i.fa-flash@class"),       //green icon + flash
+
+            //Check if has any special modifiers
+            spell: '.item@data-spell_1',
+            effect: '.item@data-effect_name',
+
+        }])
+    }])
+    ((err, result) => {
+        processScrape(programMemory, err, result, link);
+    });
+}
+
+
+function scrapeLinks(programMemory, linkList) {
+
+
+
+    //Couting scraped items;
+    programMemory.scrapingItemCounter = linkList.length;
+    //Start progress bar
+    programMemory.progressBar.start(linkList.length, 0, {
+        speed: "N/A"
+    });
+    
+    for(let link of linkList) {
+        scrapeLink(programMemory, link);
     }
 
 }
 
 import * as links from './links.js';
 
+//import * as Enquirer from 'enquirer';
+import pkg_enq from 'enquirer';
+const {MultiSelect} = pkg_enq;
+
+import pkg_bar from 'cli-progress';
+const {SingleBar} = pkg_bar;
+
+import pkg_ansi from 'ansi-colors';
+const colors = pkg_ansi;
+
+
+let programMemory = new ProgramMemory();
+
+
+const prompt = new MultiSelect({
+    name: 'value',
+    message: 'Pick items to fetch, space to select, enter to confirm',
+    limit: 7,
+    choices: [
+        { message: 'currency', name: 'currencyLinks' },
+        { message: 'taunts', name: 'tauntLinks' },
+    ],
+});
+  
+programMemory.progressBar = new SingleBar({
+    format: 'CLI Progress |' + colors.cyan('{bar}') + '| {percentage}% || {value}/{total} Chunks || Speed: {speed}',
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    hideCursor: true
+});
+
+
+
 getKeyPrice();
 
 
 process.on("keyPrice", () => {
     console.log("<++> Key price fetched = " + keyPrice);
-    scrapeLinks(links.tauntLinks);
+    prompt.run()
+        .then(answers => {
+            
+            programMemory.scrapingLinksList = answers;
+            
+            scrapeLinks(programMemory, links[programMemory.scrapingLinksList.pop()]);
+        })
+        .catch(console.error);
+
 });
 
 process.on("scrapingFinished", () => {
+    programMemory.progressBar.stop();
     programMemory.scrapingOutput.sort(compareScrapedItems);
     console.log(programMemory.scrapingOutput);
+    programMemory.scrapingOutput = [];
+
+    if(programMemory.scrapingLinksList.length > 0) {
+        scrapeLinks(programMemory, links[programMemory.scrapingLinksList.pop()]);
+    }
 });
+
+
+
+
+function cacheOutput(scrapingOutput) {
+
+}
